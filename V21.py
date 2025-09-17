f2c1dcdc64cf4a85b96c52866ed9572ac76c2e56
Ana Brito <anagpbritox@gmail.com>
2025-09-17 17:26:55 -0300

    v21
    pronto

---
+++ V21.py
@@ -0,0 +1,20 @@
+# @cikey b8c2f0e997296c4047aa90efbb14a388
+# @sid 20251174010038
+# @aid V2.1
+
+
+#begin_inputs
+
+# defina as variáveis aqui
+
+
+
+#end_inputs
+
+
+#os prints vão a partir daqui
+
+- print("{} e {} sao {} e {}".format("x, y, a, b"))
+- print("Total: {} + {}".format("x, y"))
+- print("{}, {}, {}, são números inteiros".format("a, b, c"))
+- print("{}, + 0 =".format("z"))
