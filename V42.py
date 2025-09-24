57a1913d713988b4bcf794d4c7cc0a9c5001cada
Ana Brito <anagpbritox@gmail.com>
2025-09-24 17:52:43 -0300

    Create V42.py

---
+++ V42.py
@@ -0,0 +1,19 @@
+# @cikey b8c2f0e997296c4047aa90efbb14a388
+# @sid 20251174010038
+# @aid V4.2
+
+
+#begin_inputs
+
+pessoas = 0
+
+for n in range(10):
+    pessoas(int(input("Digite a idade das pessoas: ")))
+    if pessoas >= 18
+    pessoas += 1
+
+#mantenha esse trecho. o input será manipulado aqui.
+
+#end_inputs
+
+print(pessoas)
