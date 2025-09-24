92bf6a15bd0e844a7864e0836a9a52d338d69f3b
Ana Brito <anagpbritox@gmail.com>
2025-09-24 17:50:48 -0300

    Create V41.py

---
+++ V41.py
@@ -0,0 +1,17 @@
+# @cikey b8c2f0e997296c4047aa90efbb14a388
+# @sid 20251174010038
+# @aid V4.1
+
+
+#begin_inputs
+
+x = int(input("Digite o numero: "))
+y = int(input("Digite o numero: "))
+
+#end_inputs
+
+if x > y:
+    print(x)
+
+else:
+    print(y)
