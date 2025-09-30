3c73a5e6d64d74a5ad0c0356563ee273532ba0ad
Ana Brito <anagpbritox@gmail.com>
2025-09-30 14:04:05 -0300

    Create V11.py

---
+++ V11.py
@@ -0,0 +1,30 @@
+# @cikey b8c2f0e997296c4047aa90efbb14a388
+# @sid 20251174010038
+# @aid V1.1
+
+
+#begin_inputs
+
+num6 = 6
+num4 = 4
+num20 = 20
+
+#end_inputs
+
+soma = num6 + num4
+print(soma)
+
+subtração = num4 - num6
+print(subtração)
+
+multiplicação = num6 * num4
+print(multiplicação)
+
+divisão = num20 / num6
+print(divisão)
+
+resto_da_divisão = num20 // num6
+print(resto_da_divisão)
+
+resto_da_divisão_inteira=num20 % num6
+print(resto_da_divisão_inteira)
