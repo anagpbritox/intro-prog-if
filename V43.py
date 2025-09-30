ed9f06ce77206e9af27ca3bcb259cfa113708a86
Ana Brito <anagpbritox@gmail.com>
2025-09-30 14:22:06 -0300

    Update V43.py

--- V43.py
+++ V43.py
@@ -9,11 +9,42 @@ mes = int(input("Digite o numero do mes: "))
 
 #end_inputs
 
-lista = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
 
-saída = mes - 1
+if mes == 1:
+    print('janeiro')
 
-if 1 > mes or mes > 12:
-    print('mes invalido')
+elif mes == 2:
+    print("feveiro")
+
+elif mes == 3:
+    print("março")
+
+elif mes == 4:
+    print("abril")
+
+elif mes == 5:
+    print("maio")
+
+elif mes == 6:
+    print("junho")
+
+elif mes == 7:
+    print("julho")
+
+elif mes == 8:
+    print("agosto")
+
+elif mes == 9:
+    print("setembro")
+
+elif mes == 10:
+    print("outubro")
+
+elif mes == 11:
+    print("novembro")
+
+elif mes == 12:
+    print("dezembro")
+
 else:
-    print(lista[saída])
+    print("mes invalido")
