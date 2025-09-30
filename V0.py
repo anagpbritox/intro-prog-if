6bf0062d93682f8cbfd9710f60bc1110b74f320b
Ana Brito <anagpbritox@gmail.com>
2025-09-30 14:04:02 -0300

    Create V0.py

---
+++ V0.py
@@ -0,0 +1,14 @@
+# @cikey b8c2f0e997296c4047aa90efbb14a388
+# @sid 20251174010038
+# @aid 0
+
+
+#begin_inputs
+
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o primeiro numero: "))
+
+#end_inputs
+	
+soma = num1 + num2
+print(soma)
