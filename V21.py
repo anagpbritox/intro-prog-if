870d40abb1abf22521914f0c857effa08d779119
Ana Brito <anagpbritox@gmail.com>
2025-09-30 14:04:07 -0300

    Create V21.py

---
+++ V21.py
@@ -0,0 +1,20 @@
+# @cikey b8c2f0e997296c4047aa90efbb14a388
+# @sid 20251174010038
+# @aid V2.1
+
+
+#begin_inputs
+
+a = 1
+b = 2
+c = 3
+z = 4
+x = a
+y = b
+ 
+#end_inputs
+
+print("{} e {} sao {} e {}".format(x,y,a,b))
+print("Total: {}".format(a+b))
+print("{} {} {} sao numeros inteiros".format(a,b,c))
+print("{} + 0 = {}".format(z,z+0))
