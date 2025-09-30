64644d2d5122312b3766494fb998f2c5537a9a7a
Ana Brito <anagpbritox@gmail.com>
2025-09-30 13:55:29 -0300

    Create V34.py

---
+++ V34.py
@@ -0,0 +1,15 @@
+# @cikey b8c2f0e997296c4047aa90efbb14a388
+# @sid 20251174010038
+# @aid V3.4
+
+
+#begin_inputs
+
+# programa sem entrada (input)
+
+#end_inputs
+
+for ano in range(2017, 2021):
+    for mes in range(1, 13):
+        for dia in range(1, 31):
+            print("{}/{}/{}".format(dia,mes,ano))
